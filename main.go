83489539ff1f8bbd2ced0148e90558a9dfa534f1
Sebastian Letelier <sebalete10@gmail.com>
2023-03-22 16:51:42 -0300

    fiber setup

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
+	
+	app := fiber.New()
+	app.Listen(":3000")
+	fmt.Println("Server on port 3000")
 
 }
+
+//go run .
